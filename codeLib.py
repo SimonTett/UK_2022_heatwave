f3a450fd346b2e4ae499efdeffcf83808b2b49b2
Simon Tett <tetts@sci1.jasmin.ac.uk>
2022-07-25 10:10:24 +0100

    Added some text to Readme.md saying where data was Added a library contining ifnromation on region and where data live.

---
+++ codeLib.py
@@ -0,0 +1,4 @@
+# Standard stuff for code
+import pathlib
+region=dict(llat=slice(51.25,54.0,lon=slice(-3.5,0.5)
+reference_dir = pathlib.Path('/badc/eucleia/data/EUCLEIA/output/MOHC/HadGEM3-A-N216/historical/day/atmos/day/r1i1p1/latest/') # EUCLEAI data
